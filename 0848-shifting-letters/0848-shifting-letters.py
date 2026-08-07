573e5c8cec4f875017c40174ee7088cda8aa2b9c
Confiance <isingizweconfy@gmail.com>
2026-08-07 20:56:11 +0200

    Time: 103 ms (29.45%), Space: 31.7 MB (22.93%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.py
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -1,18 +1,16 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
         diff_arr = [0] * (len(s) + 1)
+        diff_arr[0] = sum(shifts)
 
         for idx in range(len(shifts)):
-            diff_arr[0] += shifts[idx]
             diff_arr[idx + 1] -= shifts[idx]
 
-        prefix = 0
+        shift = 0
         result = []
 
         for idx in range(len(s)):
-            prefix += diff_arr[idx]
-            shift = prefix
-
+            shift += diff_arr[idx]
             new_char = chr((ord(s[idx]) - 97 + shift) % 26 + 97)
 
             result.append(new_char)
